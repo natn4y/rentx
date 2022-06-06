658049a09b85ad87933f6683e5863a3e1c527ef1
Natanael Araújo <natanael@hotmail.co.jp>
2022-06-06 16:32:09 -0300

    screen(Home): Configurado o Header e o  StatusBar

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StatusBar } from 'react-native'
+
+import { Container, Header } from './styles'
+
+export function Home() {
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header></Header>
+    </Container>
+  )
+}
